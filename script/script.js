e45fc92da5aade4052a458464750899db96fd157
zdmari1 <110204951+zdmari1@users.noreply.github.com>
2023-09-11 13:40:25 -0300

    11/09

--- script/script.js
+++ script/script.js
@@ -1,3 +1,19 @@
 function alo_mundo () {
     alert ("Projeto Iniciado!")
 }
+escrever = msg => alert(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+raiz = a => Math.sqrt(a);
+equacao2Grau = (a,b,c) =>{
+    let delta = sub(mult(b,b),mult(4,mult(a,c)));
+    if (delta<0) return "não possui raiz real.";
+    if (delta == 0)return div(-b,mult(2,a));
+    return "x1 = " + div(soma (-b,raiz(delta)), mult(2,a)) + 
+    "x2 = " + div(soma (b,raiz(delta)), mult(2,a));
+}
+
+//tentando... 1000  x 22/100 + 1000
+escrever(equacao2Grau(3,-2,-1));
