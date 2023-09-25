b20964ee0bdd62a0913e2449be842c23b679b4c0
zdmari1 <110204951+zdmari1@users.noreply.github.com>
2023-09-25 10:47:18 -0300

    25/09

--- script.js
+++ script.js
@@ -1,35 +1,78 @@
-function alo_mundo () {
-    alert ("Projeto Iniciado!")
-}
-escrever = msg => alert(msg);
-soma = (a,b) => a + b;
+let a = "";
+let b = "";
+let valor = "";
+let executar = "";
+let temPonto = false;
+
+soma = (a,b) => Number(a) + Number(b);
 sub = (a,b) => a - b;
 mult = (a,b) => a * b;
 div = (a,b) => a / b;
 raiz = a => Math.sqrt(a);
+
 equacao2Grau = (a,b,c) =>{
     let delta = sub(mult(b,b),mult(4,mult(a,c)));
-    if (delta<0) return "não possui raiz real.";
-    if (delta == 0)return div(-b,mult(2,a));
-    return "x1 = " + div(soma (-b,raiz(delta)), mult(2,a)) + 
-    "x2 = " + div(soma (b,raiz(delta)), mult(2,a));
+    if (delta < 0) return "Não possui raiz Real.";
+    if (delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
+    return "x1 = " + div(soma(-b,raiz(delta)),mult(2,a)) + 
+           " x2 = " + div(sub(-b,raiz(delta)),mult(2,a));
 }
 
-let a = 0;
-let b = 0;
-let resultado = 0;
-let executar = ""
+function mostrar_resultado(){
+    document.getElementById("resultado").value = valor;
+}
 
-calcular = () => {
-    if (executar != ""){
-        if (executar = "soma") mostrar_resultado (soma(a,b));
-        if (executar = "sub") mostrar_resultado (sub(a,b));
-        if (executar = "div") mostrar_resultado (div(a,b));
-        if (executar = "mult") mostrar_resultado (mult(a,b));
+function calcular(){
+    if(executar != ""){
+        b = valor;
+        if(executar == "soma") valor = soma(Number(a),Number(b));
+        if(executar == "sub") valor = sub(a,b);
+        if(executar == "div") valor = div(a,b);
+        if(executar == "mult") valor = mult(a,b);
+        if(executar == "porc") valor = porc(div(a/100),b);
+        if(executar == "raiz") valor = raiz(a,b);
+        mostrar_resultado();
+        executar = "";
+        a = "";
+        b = "";
+        valor = "";
+        temPonto = false;
     }
 }
+function desliga()
+{
+    if (desligada) {
+        zerar();
+       
+    } else {
+        mostrar_resultado();
+        zerar = "";
+    }
+    desligada = !desligada;
+}
+function zerar (){
+    a = "";
+    b = "";
+    valor = "0";
+    executar ="";
+    mostrar_resultado();
+    valor ="";
+}
+function operacao(op){
+    executar = op;
+    a = valor;
+    valor = "";
+}
 
-operacao = op => executar = op;
-digitando = (num) {
-alert (num);
+function digitando(tecla){
+   if (tecla == "."){
+       if(!temPonto) {
+         valor = valor + tecla;
+         mostrar_resultado();
+         temPonto = true;
+       }
+       return;
+   }
+   valor = valor + tecla;
+   mostrar_resultado();
 }
